c1e951c89d4c8aea2100975848a334bbe474b103
mxmo0rhuhn <code@schrimpf.ch>
2016-08-20 13:20:54 +0200

    complete task 10

--- haskell/coding_challenges/1_to_10.hs
+++ haskell/coding_challenges/1_to_10.hs
@@ -101,3 +101,16 @@ pack (x:xs) = aux [x] xs
     aux packed (x:xs)
       | (head packed) == x = ( aux (packed ++ [x]) xs )
       | otherwise          = [packed] ++ ( aux [x] xs )
+
+-- 10
+--(*) Run-length encoding of a list. Use the result of problem P09 to implement the 
+-- so-called run-length encoding data compression method. Consecutive duplicates of 
+-- elements are encoded as lists (N E) where N is the number of duplicates of the 
+-- element E. 
+-- 
+-- > encode "aaaabccaadeeee"
+-- [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+
+encode :: String -> [(Int, Char)]
+encode [] = []
+encode xs = map ( \x -> ( length x , head x ) ) (pack xs)
